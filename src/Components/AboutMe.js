e427c789823461b10416e4f194857eb0f4d9d5a7
Devesh Narayan Choukiker <choukiker.devesh30@gmail.com>
2023-04-04 12:17:26 +0530

    changes

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./AboutMe.css";
+const AboutMe = () => {
+  return (
+    <>
+      <div className="about">
+        <h2>About me </h2>
+        <p>
+          Magna voluptate fugiat reprehenderit est. Esse incididunt esse laborum
+          esse eu Lorem et. Nostrud ipsum non occaecat ut proident tempor nulla
+          dolor quis consequat nulla. Veniam nostrud ipsum reprehenderit id non
+          quis culpa. Ad eiusmod elit reprehenderit fugiat officia do occaecat
+          in exercitation eiusmod excepteur minim consequat reprehenderit. Nulla
+          eiusmod ad velit excepteur enim. Laboris nisi et eu dolore ea dolore
+          ipsum adipisicing amet. Eu voluptate cillum aute aliqua amet sit
+          cillum excepteur. Voluptate quis Lorem pariatur nostrud officia veniam
+          et. Deserunt elit sit laborum elit reprehenderit quis est laboris
+          voluptate sunt labore sit ad. Commodo do aliqua cillum occaecat. Qui
+          ullamco sit pariatur non id nisi officia. Ad culpa reprehenderit
+          cupidatat in consectetur adipisicing id. Est cupidatat esse minim duis
+          exercitation laboris officia. Nisi mollit nisi nostrud in ullamco
+          magna reprehenderit fugiat. Occaecat aute qui mollit magna fugiat
+          duis. Duis excepteur deserunt qui commodo. Esse duis officia aliqua
+          cupidatat dolor aliqua voluptate nostrud officia non consequat mollit
+          aute anim. Aute pariatur voluptate cupidatat nostrud. Quis consequat
+          excepteur enim esse ea quis tempor exercitation deserunt voluptate
+          culpa cupidatat. Aute anim commodo fugiat deserunt excepteur ut. Non
+          ex consequat aliqua laboris exercitation pariatur sit exercitation
+          deserunt nostrud ut officia adipisicing. Ut veniam deserunt incididunt
+          dolore voluptate non duis. Nulla duis et consectetur nisi mollit velit
+          eiusmod dolore veniam esse minim aute esse dolore. Id minim ut
+          voluptate exercitation esse. Occaecat ut aliqua ipsum ex dolor ut anim
+          sit ipsum enim mollit velit enim excepteur. Amet ut id enim
+          reprehenderit quis voluptate non mollit ut ex dolore excepteur. Minim
+          ipsum eu id laboris enim enim quis. Laborum nostrud in non ex irure
+          nulla ea. Enim id dolore pariatur ea. Pariatur irure esse veniam
+          reprehenderit est nostrud aute eiusmod adipisicing. Sunt non Lorem
+          duis ea in consequat non eu sit. Incididunt dolore laborum amet
+          pariatur qui elit aliqua veniam nisi velit officia exercitation enim.
+          Laborum mollit proident do enim magna ipsum veniam mollit. Commodo
+          aliquip occaecat duis qui ad esse labore duis anim. Consequat anim
+          amet ad proident officia sint adipisicing amet duis irure ea. Ex
+          adipisicing pariatur Lorem sit laboris. Velit tempor qui quis ea duis
+          cupidatat reprehenderit reprehenderit est consequat tempor ut
+          excepteur ut. Magna velit eu Lorem et cillum occaecat aliquip
+          cupidatat enim reprehenderit tempor labore fugiat est. Esse velit
+          culpa occaecat minim cillum in irure. Laboris sit elit culpa nisi et.
+          Incididunt do qui officia proident.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default AboutMe;
--- src/routes/About.js
+++ src/routes/About.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import HeroImage2 from "../Components/HeroImage2";
+import AboutMe from "../Components/AboutMe";
 
 const About = () => {
   return (
     <>
       <Navbar />
       <HeroImage2 heading="ABOUT" text="I AM A React Developer" />
+      <AboutMe />
       <Footer />
     </>
   );
